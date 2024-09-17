9147344eb93dd14ff6fe6f21ae5c1e6fdd047e08
MRMajewski <mmajewski333@gmail.com>
2024-09-17 11:39:58 +0200

    Small grid manager refactor

--- Assets/Scripts/GridManager.cs
+++ Assets/Scripts/GridManager.cs
@@ -7,12 +7,14 @@ public class Node
 {
     public Vector2 Position;
     public bool IsOccupied;
-    public List<int> Neighbors; // Lista s¹siadów
+    public List<int> Neighbors;
+    public bool IsGoalNode;
 
-    public Node(Vector2 position)
+    public Node(Vector2 position, bool isGoal = false)
     {
         Position = position;
         IsOccupied = false;
+        IsGoalNode = isGoal;
         Neighbors = new List<int>();
     }
 
@@ -26,9 +28,9 @@ public class Node
 }
 
 public class GridManager : MonoBehaviour
-{
-    public float nodeSpacing = 1f;
-    public Vector2 offset;
+{  [SerializeField]
+    private float nodeSpacing = 1f;
+    private Vector2 offset;
     [SerializeField]
     private List<Node> nodes = new List<Node>();
     [SerializeField]
@@ -39,9 +41,9 @@ public class GridManager : MonoBehaviour
         offset = new Vector2(gridWidth / -2f, gridHeight / -2f);
         nodes.Clear();
         goalNodes.Clear();
+
         int totalNodes = (gridWidth + 1) * (gridHeight + 1);
 
-        // Generowanie wêz³ów dla siatki (+1 w szerokoœci i wysokoœci)
         for (int x = 0; x <= gridWidth; x++)
         {
             for (int y = 0; y <= gridHeight; y++)
@@ -49,62 +51,61 @@ public class GridManager : MonoBehaviour
                 Vector2 position = new Vector2(x * nodeSpacing + offset.x, y * nodeSpacing + offset.y);
                 Node newNode = new Node(position);
                 nodes.Add(newNode);
-                // Opcjonalnie rysowanie wêz³ów
-                // Debug.DrawLine(position - Vector2.right * 0.5f, position + Vector2.right * 0.5f, Color.red, 100f);
-                // Debug.DrawLine(position - Vector2.up * 0.5f, position + Vector2.up * 0.5f, Color.red, 100f);
             }
         }
 
-        // Generowanie wêz³ów bramkowych
-        int halfGridWidth = gridWidth / 2;
-        int halfGoalWidth = goalWidth / 2;
-        Vector2 topGoalCenter = new Vector2(halfGridWidth * nodeSpacing + offset.x, nodeSpacing - offset.y);
-        Vector2 bottomGoalCenter = new Vector2(halfGridWidth * nodeSpacing + offset.x, -nodeSpacing + offset.y);
+        Vector2 topGoalCenter = new Vector2(gridWidth / 2 * nodeSpacing + offset.x, nodeSpacing - offset.y);
+        Vector2 bottomGoalCenter = new Vector2(gridWidth / 2 * nodeSpacing + offset.x, -nodeSpacing + offset.y);
 
-        goalNodes.Add(new Node(topGoalCenter));
-        goalNodes.Add(new Node(bottomGoalCenter));
+        Node topGoal = new Node(topGoalCenter, true);
+        Node bottomGoal = new Node(bottomGoalCenter, true);
 
-        // Przypisywanie s¹siadów
+        goalNodes.Add(topGoal);
+        goalNodes.Add(bottomGoal);
+
+        nodes.Add(topGoal);
+        nodes.Add(bottomGoal);
+
         AssignNeighbors(gridWidth, gridHeight);
     }
 
     private void AssignNeighbors(int gridWidth, int gridHeight)
     {
-        // Najpierw przypisujemy s¹siadów
         for (int i = 0; i < nodes.Count; i++)
         {
             Node currentNode = nodes[i];
-            Vector2 currentPosition = currentNode.Position;
 
             for (int j = 0; j < nodes.Count; j++)
             {
-                if (i == j) continue; // Nie porównuj wêz³a z samym sob¹
+                if (i == j) continue;
 
                 Node neighborNode = nodes[j];
-                Vector2 neighborPosition = neighborNode.Position;
+                Vector2 distance = currentNode.Position - neighborNode.Position;
 
-                // Oblicz odleg³oœci
-                float distanceX = Mathf.Abs(currentPosition.x - neighborPosition.x);
-                float distanceY = Mathf.Abs(currentPosition.y - neighborPosition.y);
-
-                // Odleg³oœæ po skosie
-                float distanceDiagonal = Mathf.Sqrt(distanceX * distanceX + distanceY * distanceY);
-
-                // Sprawdzamy, czy s¹siad znajduje siê w odleg³oœci równej lub mniejszej ni¿ `nodeSpacing`
-                bool isNeighbor = (distanceX <= nodeSpacing + 0.1f && distanceY <= nodeSpacing + 0.1f) ||
-                                  (distanceDiagonal <= nodeSpacing * Mathf.Sqrt(2) + 0.1f);
-
-                if (isNeighbor)
+                if (distance.magnitude <= nodeSpacing * Mathf.Sqrt(2) + 0.1f)
                 {
                     currentNode.AddNeighbor(j);
                 }
             }
         }
 
-        // Modyfikujemy s¹siedztwo wêz³ów na krawêdziach
         ModifyAdjacencyForEdgeNodes(gridWidth, gridHeight);
 
-        // Nastêpnie rysujemy linie miêdzy wêz³ami na podstawie listy s¹siadów
+        foreach (var goalNode in goalNodes)
+        {
+            for (int i = 0; i < nodes.Count; i++)
+            {
+                Node currentNode = nodes[i];
+                Vector2 distance = goalNode.Position - currentNode.Position;
+
+                if (distance.magnitude <= nodeSpacing * Mathf.Sqrt(2) + 0.1f)
+                {
+                    goalNode.AddNeighbor(i);
+                    currentNode.AddNeighbor(nodes.IndexOf(goalNode));
+                }
+            }
+        }
+
         DrawConnections();
     }
 
@@ -113,18 +114,15 @@ public class GridManager : MonoBehaviour
         for (int i = 0; i < nodes.Count; i++)
         {
             Node currentNode = nodes[i];
-            Vector2 currentPosition = currentNode.Position;
 
             foreach (int neighborIndex in currentNode.Neighbors)
             {
                 Node neighborNode = nodes[neighborIndex];
-                Vector2 neighborPosition = neighborNode.Position;
-
-                // Rysowanie linii miêdzy wêz³ami
-                Debug.DrawLine(currentPosition, neighborPosition, Color.red, 100f);
+                Debug.DrawLine(currentNode.Position, neighborNode.Position, Color.red, 100f);
             }
         }
     }
+
     private void ModifyAdjacencyForEdgeNodes(int gridWidth, int gridHeight)
     {
         float edgeX = offset.x;
@@ -135,72 +133,32 @@ public class GridManager : MonoBehaviour
         for (int i = 0; i < nodes.Count; i++)
         {
             Node currentNode = nodes[i];
-            Vector2 currentPosition = currentNode.Position;
 
-            // Sprawdzamy, czy wêze³ jest na lewej krawêdzi siatki
-            if (Mathf.Approximately(currentPosition.x, edgeX))
+            if (Mathf.Approximately(currentNode.Position.x, edgeX) || Mathf.Approximately(currentNode.Position.x, edgeXMax))
             {
-                // Usuñ powi¹zania z s¹siadami w osi X
-                for (int j = currentNode.Neighbors.Count - 1; j >= 0; j--)
-                {
-                    int neighborIndex = currentNode.Neighbors[j];
-                    Node neighborNode = nodes[neighborIndex];
-                    if (Mathf.Approximately(neighborNode.Position.x, edgeX))
-                    {
-                        // Usuñ po³¹czenie, jeœli s¹siad jest równie¿ na lewej krawêdzi
-                        currentNode.Neighbors.RemoveAt(j);
-                    }
-                }
-            }
-            // Sprawdzamy, czy wêze³ jest na prawej krawêdzi siatki
-            else if (Mathf.Approximately(currentPosition.x, edgeXMax))
-            {
-                // Usuñ powi¹zania z s¹siadami w osi X
-                for (int j = currentNode.Neighbors.Count - 1; j >= 0; j--)
-                {
-                    int neighborIndex = currentNode.Neighbors[j];
-                    Node neighborNode = nodes[neighborIndex];
-                    if (Mathf.Approximately(neighborNode.Position.x, edgeXMax))
-                    {
-                        // Usuñ po³¹czenie, jeœli s¹siad jest równie¿ na prawej krawêdzi
-                        currentNode.Neighbors.RemoveAt(j);
-                    }
-                }
+                RemoveEdgeNeighbors(currentNode, n => Mathf.Approximately(n.Position.x, currentNode.Position.x));
             }
 
-            // Sprawdzamy, czy wêze³ jest na górnej krawêdzi siatki
-            if (Mathf.Approximately(currentPosition.y, edgeY))
+            if (Mathf.Approximately(currentNode.Position.y, edgeY) || Mathf.Approximately(currentNode.Position.y, edgeYMax))
             {
-                // Usuñ powi¹zania z s¹siadami w osi Y
-                for (int j = currentNode.Neighbors.Count - 1; j >= 0; j--)
-                {
-                    int neighborIndex = currentNode.Neighbors[j];
-                    Node neighborNode = nodes[neighborIndex];
-                    if (Mathf.Approximately(neighborNode.Position.y, edgeY))
-                    {
-                        // Usuñ po³¹czenie, jeœli s¹siad jest równie¿ na górnej krawêdzi
-                        currentNode.Neighbors.RemoveAt(j);
-                    }
-                }
-            }
-            // Sprawdzamy, czy wêze³ jest na dolnej krawêdzi siatki
-            else if (Mathf.Approximately(currentPosition.y, edgeYMax))
-            {
-                // Usuñ powi¹zania z s¹siadami w osi Y
-                for (int j = currentNode.Neighbors.Count - 1; j >= 0; j--)
-                {
-                    int neighborIndex = currentNode.Neighbors[j];
-                    Node neighborNode = nodes[neighborIndex];
-                    if (Mathf.Approximately(neighborNode.Position.y, edgeYMax))
-                    {
-                        // Usuñ po³¹czenie, jeœli s¹siad jest równie¿ na dolnej krawêdzi
-                        currentNode.Neighbors.RemoveAt(j);
-                    }
-                }
+                RemoveEdgeNeighbors(currentNode, n => Mathf.Approximately(n.Position.y, currentNode.Position.y));
             }
         }
     }
 
+    private void RemoveEdgeNeighbors(Node node, Predicate<Node> condition)
+    {
+        for (int j = node.Neighbors.Count - 1; j >= 0; j--)
+        {
+            int neighborIndex = node.Neighbors[j];
+            Node neighborNode = nodes[neighborIndex];
+
+            if (condition(neighborNode))
+            {
+                node.Neighbors.RemoveAt(j);
+            }
+        }
+    }
 
     void OnDrawGizmos()
     {
@@ -212,7 +170,6 @@ public class GridManager : MonoBehaviour
                 Gizmos.DrawSphere(new Vector3(node.Position.x, node.Position.y, 0), 0.1f);
             }
 
-            // Rysowanie wêz³ów bramkowych
             if (goalNodes != null)
             {
                 Gizmos.color = Color.green;
@@ -222,9 +179,6 @@ public class GridManager : MonoBehaviour
                 }
             }
         }
-
-        // Opcjonalne rysowanie po³¹czeñ w edytorze
-        // DrawConnections();
     }
 
     private Vector2 GetClosestNodePosition(Vector2 position)
@@ -232,7 +186,7 @@ public class GridManager : MonoBehaviour
         Node closestNode = null;
         float closestDistance = Mathf.Infinity;
 
-        foreach (var node in GetAllNodes())
+        foreach (var node in nodes)
         {
             float distance = Vector2.Distance(position, node.Position);
             if (distance < closestDistance)
@@ -245,13 +199,6 @@ public class GridManager : MonoBehaviour
         return closestNode != null ? closestNode.Position : position;
     }
 
-    public List<Node> GetAllNodes()
-    {
-        return nodes;
-    }
-
-    public List<Node> GetGoalNodes()
-    {
-        return goalNodes;
-    }
+    public List<Node> GetAllNodes() => nodes;
+    public List<Node> GetGoalNodes() => goalNodes;
 }
