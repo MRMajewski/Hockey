2d18aaa71c3aaa4d92a297b847fd5c69d4c93fe5
MRMajewski <mmajewski333@gmail.com>
2024-09-19 10:16:45 +0200

    Added new script for help in debugging

---
+++ Assets/Scripts/NodeDebuggerGizmos.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NodeDebuggerGizmos : MonoBehaviour
+{
+    public BallMovement ballMovement; // Referencja do skryptu BallMovement
+    public float gizmoRadius = 0.5f; // Promieñ sfery gizmo
+
+    private void OnDrawGizmos()
+    {
+        if (ballMovement != null)
+        {
+            Node currentNode = ballMovement.GetCurrentNode();
+            if (currentNode != null)
+            {
+                // Ustaw kolor gizmo na zielony
+                Gizmos.color = Color.green;
+
+                // Rysuj sferê gizmo na pozycji aktualnego wêz³a
+                Gizmos.DrawSphere(currentNode.Position, gizmoRadius);
+            }
+        }
+    }
+}
